2cfa90a0e9df4db2b3281ca734a892354914331e
Konstantin <wusnoop@gmail.com>
2022-03-29 18:31:11 +0300

    add

--- index.php
+++ index.php
@@ -1,41 +1,67 @@
 <?php
-
-/*$arr = [50.25, 20.38, 50.78, 75.75, 50];
-$arr2 = [];
-foreach ($arr as $v) {
-    $arr2[] = $v * 0.77;
-
-   }
-foreach ($arr2 as $v){
-    echo round($v, 2).'<br>' ;
-}*/
-$max = false;
-$min = false;
-$rand = false;
-$num1 = false;
-$num2 = false;
-if(isset($_POST['number'])){
-    $num1 = $_POST['num1'] ?? false;
-    $num2 = $_POST['num2'] ?? false;
-    if ($num1 !== false && $num2 !== false && is_numeric($num1) && is_numeric($num2))
-        $min = min($num2, $num1);
-    $max = max($num2, $num1);
-    $rand = mt_rand($min, $max);
-
-
+class Point {
+    private $x;
+    private $y;
+    public static $counter;
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        self::$counter++;
+    }
+    public function setX($x){
+        $this->x = $x;
+    }
+    public function getX(){
+        return $this->x;
 }
-?>
-<p> Number is :<?=$rand?></p>
-<form name="number" action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
-    <div>
-        First num: <input type="text" name="num1" value="<?=$num1?>"/>
-    </div>
-    <div>
-        Second num: <input type="text" name="num2" value="<?=$num2?>"/>
-    </div>
-    <div>
-        Entire: <input type="submit" name="number" value="random"/>
-    </div>
+    public function setY($y){
+        $this->y = $y;
+    }
+    public function getY(){
+        return $this->y;
+    }
+    public function __destruct(){
+        echo 'Delete this object'.'<br />';
+    }
+    public static function getCounter(){
+        return self::$counter;
+}
+    public function __toString(){
 
-</form>
+        return  "Точка с координатами ($this->x, $this->y)".'<br />';
+    }
+    public function __get($name){
+        if (isset($this->x[$name])) return $this->x[$name];
+        return 'Класс Point работает только в двумерном пространстве';
+    }
+    public function __set($name, $value){
+        $this->x[$name] = $value;
+    }
+    public function __call($method, $args){
+        echo 'Класс Point работает только в двумерном пространстве';
+    }
+}
 
+$point = new Point(3, 5);
+$point2 = new Point(4, 12);
+$point3 = new Point(1, 6);
+
+echo $point->getX().'<br />';
+echo $point->getY().'<br />';
+
+$point->setX(15);
+$point->setY(23);
+echo $point->getX().'<br />';
+echo $point->getY().'<br />';
+echo $point2->getX().'<br />';
+echo $point2->getY().'<br />';
+echo $point3->getX().'<br />';
+echo $point3->getY().'<br />';
+echo "--------------------".'<br />';
+echo Point::getCounter().'<br />';
+echo $point.'<br />';
+echo $point2.'<br />';
+echo $point3.'<br />';
+echo $point->z.'<br />';
+echo $point->setZ().'<br />';
